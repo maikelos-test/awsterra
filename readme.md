07ef6bffcadac687adaeb424e55918edfff5b1e6
Michal Wesolowski <michal.wesolowski@bluesmithis.com>
2024-08-08 08:42:52 +0100

    first

---
+++ readme.md
@@ -0,0 +1,12 @@
+https://github.com/aws-actions/configure-aws-credentials
+
+    - name: Configure AWS Credentials
+      uses: aws-actions/configure-aws-credentials@v4
+      with:
+        aws-access-key-id: ${{ secrets.AWS_ACCESS_KEY_ID }}
+        aws-secret-access-key: ${{ secrets.AWS_SECRET_ACCESS_KEY }}
+        aws-region: us-east-2
+        role-to-assume: ${{ secrets.AWS_ROLE_TO_ASSUME }}
+        role-external-id: ${{ secrets.AWS_ROLE_EXTERNAL_ID }}
+        role-duration-seconds: 1200
+        role-session-name: MySessionName
---
+++ terraform/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>5.61"
+    }
+  }
+
+  required_version = ">=0.14.9"
+
+  backend "s3" {
+    bucket     = "m272-terraformstate"
+    key        = "m272-terrastate"
+    region     = "eu-north-1"
+    access_key = "AKIAQLSIVCPPFRLVIGQB"
+    secret_key = ""
+    dynamodb_table = "m272-terra-state"
+  }
+
+}
+
+provider "aws" {
+  region     = "eu-north-1"
+  access_key = "AKIAQLSIVCPPFRLVIGQB"
+  secret_key = ""
+}
+
+resource "aws_s3_bucket" "this" {
+  bucket = "m272-terra-test"
+}
